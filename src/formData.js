28bf4e4fdb09dacfba10808a1ac13602185b7ff6
outlawstar4761 <outlawstar4761@gmail.com>
2025-04-20 16:02:35 -0500

    fixing formdata loading

--- src/formData.js
+++ src/formData.js
@@ -1,13 +1,20 @@
 let FormDataConstructor;
 
-if(typeof window !== 'undefined' && typeof window.FormData !== 'undefined'){
-  //Browser
-  FormDataConstructor = window.FormData;
-}else{
-  //Node
-  FormDataConstructor = (await import('form-data')).default;
-}
-
-export function createFormData(){
+export async function createFormData(){
+  if(!FormDataConstructor){
+    if(typeof window !== 'undefined' && typeof window.FormData !== 'undefined'){
+      //Browser
+      FormDataConstructor = window.FormData;
+    }else{
+      //Node
+      FormDataConstructor = (await import('form-data')).default;
+    }
+  }
   return new FormDataConstructor();
 }
+
+
+
+// export function createFormData(){
+//   return new FormDataConstructor();
+// }
